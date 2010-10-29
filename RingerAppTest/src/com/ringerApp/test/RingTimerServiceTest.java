958a6e2ebd5898ec82c66ab7656b87282c14d704
Greg Heartsfield <scsibug@imap.cc>
2010-10-28 20:25:07 -0500

    Added test for binding to the RingTimerService.

--- RingerAppTest/src/com/ringerApp/test/RingTimerServiceTest.java
+++ RingerAppTest/src/com/ringerApp/test/RingTimerServiceTest.java
@@ -2,9 +2,11 @@ package com.ringerApp.test;
 
 import com.ringerApp.RingTimerService;
 import android.content.Intent;
+import android.os.IBinder;
 
 import android.test.ServiceTestCase;
 import android.test.suitebuilder.annotation.SmallTest;
+import android.test.suitebuilder.annotation.MediumTest;
 
 public class RingTimerServiceTest extends ServiceTestCase<RingTimerService> {
 
@@ -27,6 +29,16 @@ public class RingTimerServiceTest extends ServiceTestCase<RingTimerService> {
             Intent startIntent = new Intent();
             startIntent.setClass(getContext(), RingTimerService.class);
             startService(startIntent); 
+        }
+
+        /**
+         * Test binding to service
+         */
+        @MediumTest
+        public void testBindable() {
+            Intent startIntent = new Intent();
+            startIntent.setClass(getContext(), RingTimerService.class);
+            IBinder service = bindService(startIntent); 
         }
 
 	@SmallTest
