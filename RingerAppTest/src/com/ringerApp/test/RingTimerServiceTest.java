d4b760d67c78c01a0e655ad8f9194766de23bf67
Greg Heartsfield <scsibug@imap.cc>
2010-10-28 20:23:03 -0500

    Added test for starting RingTimerService.

--- RingerAppTest/src/com/ringerApp/test/RingTimerServiceTest.java
+++ RingerAppTest/src/com/ringerApp/test/RingTimerServiceTest.java
@@ -1,6 +1,7 @@
 package com.ringerApp.test;
 
 import com.ringerApp.RingTimerService;
+import android.content.Intent;
 
 import android.test.ServiceTestCase;
 import android.test.suitebuilder.annotation.SmallTest;
@@ -8,10 +9,31 @@ import android.test.suitebuilder.annotation.SmallTest;
 public class RingTimerServiceTest extends ServiceTestCase<RingTimerService> {
 
 	public RingTimerServiceTest() {
-		super(com.ringerApp.RingTimerService.class);
-		// TODO Auto-generated constructor st
+		super(RingTimerService.class);
 	}
+        protected void setUp() throws Exception {
+            super.setUp();
+        }
 	
+        @SmallTest
+        public void testPreconditions() {
+        }
+
+        /**
+         * Test basic startup/shutdown of Service
+         */
+        @SmallTest
+        public void testStartable() {
+            Intent startIntent = new Intent();
+            startIntent.setClass(getContext(), RingTimerService.class);
+            startService(startIntent); 
+        }
+
+	@SmallTest
+	public void testRingerSleep(){
+            assertTrue(true);
+	}
+
 	@SmallTest
 	public void testTurnOffRinger(){
 		assertTrue(true);
