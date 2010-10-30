a83a9b9e21e1bc34ca5918990d30399172e8cf8b
Greg Heartsfield <scsibug@imap.cc>
2010-10-30 15:44:57 -0500

    Work on testing local services.

--- RingerAppTest/src/com/ringerApp/test/RingTimerServiceTest.java
+++ RingerAppTest/src/com/ringerApp/test/RingTimerServiceTest.java
@@ -1,8 +1,13 @@
 package com.ringerApp.test;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.ComponentName;
 import com.ringerApp.RingTimerService;
 import android.content.Intent;
 import android.os.IBinder;
+import android.content.ServiceConnection;
+import android.media.AudioManager;
 
 import android.test.ServiceTestCase;
 import android.test.suitebuilder.annotation.SmallTest;
@@ -38,12 +43,20 @@ public class RingTimerServiceTest extends ServiceTestCase<RingTimerService> {
         public void testBindable() {
             Intent startIntent = new Intent();
             startIntent.setClass(getContext(), RingTimerService.class);
-            IBinder service = bindService(startIntent); 
+            IBinder service = bindService(startIntent);
+            
         }
 
 	@SmallTest
 	public void testRingerSleep(){
-            assertTrue(true);
+            AudioManager mAudio = (AudioManager) getContext().getSystemService(Activity.AUDIO_SERVICE);
+            mAudio.setRingerMode(AudioManager.RINGER_MODE_NORMAL);
+            Binding binding = new Binding();
+            binding.doBindService();
+            RingTimerService rts = binding.getBoundService();
+            assertNotNull(rts);
+            rts.ringerSleep(1);
+            assertEquals(AudioManager.RINGER_MODE_SILENT, mAudio.getRingerMode());
 	}
 
 	@SmallTest
@@ -51,4 +64,51 @@ public class RingTimerServiceTest extends ServiceTestCase<RingTimerService> {
 		assertTrue(true);
 	}
 
+  public static class Binding extends Activity {
+        private boolean mIsBound;
+
+        private RingTimerService mBoundService;
+
+        private ServiceConnection mConnection = new ServiceConnection() {
+            public void onServiceConnected(ComponentName className, IBinder service) {
+                // This is called when the connection with the service has been
+                // established, giving us the service object we can use to
+                // interact with the service.  Because we have bound to a explicit
+                // service that we know is running in our own process, we can
+                // cast its IBinder to a concrete class and directly access it.
+                mBoundService = ((RingTimerService.RingTimerBinder)service).getService();
+            }
+
+            public void onServiceDisconnected(ComponentName className) {
+                // This is called when the connection with the service has been
+                // unexpectedly disconnected -- that is, its process crashed.
+                // Because it is running in our same process, we should never
+                // see this happen.
+                mBoundService = null;
+            }
+        };
+
+        void doBindService() {
+            // Establish a connection with the service.  We use an explicit
+            // class name because we want a specific service implementation that
+            // we know will be running in our own process (and thus won't be
+            // supporting component replacement by other applications).
+            bindService(new Intent(Binding.this, 
+                    RingTimerService.class), mConnection, Context.BIND_AUTO_CREATE);
+            mIsBound = true;
+        }
+
+        RingTimerService getBoundService() {
+            while (mBoundService == null) {}
+            return mBoundService;
+        }
+
+        void doUnbindService() {
+            if (mIsBound) {
+                // Detach our existing connection.
+                unbindService(mConnection);
+                mIsBound = false;
+            }
+        }
+    }
 }
--- ringerApp/src/com/ringerApp/RingTimerService.java
+++ ringerApp/src/com/ringerApp/RingTimerService.java
@@ -2,23 +2,46 @@ package com.ringerApp;
 
 import android.app.Activity;
 import android.app.Service;
-import android.content.Context;
 import android.content.Intent;
 import android.media.AudioManager;
+import android.os.Binder;
 import android.os.IBinder;
 
 public class RingTimerService extends Service {
 
+        /**
+         * Class for clients to access.  Because we know this service always
+         * runs in the same process as its clients, we don't need to deal with
+         * IPC.
+         */
+        public class RingTimerBinder extends Binder {
+            public RingTimerService getService() {
+                return RingTimerService.this;
+            }
+        }
+
+        // This is the object that receives interactions from clients.  See
+        // RemoteService for a more complete example.
+        private final IBinder mBinder = new RingTimerBinder();
+
 	@Override
 	public IBinder onBind(Intent arg0) {
-		// TODO Auto-generated method stub
-		return null;
+                return mBinder;
+	}
+
+	public void ringerSleep(int seconds) {
+		AudioManager mAudio = (AudioManager) getSystemService(Activity.AUDIO_SERVICE);
+		mAudio.setRingerMode(AudioManager.RINGER_MODE_SILENT);
+	}	
+
+	public void turnOffRinger(Intent intent){
+		AudioManager mAudio = (AudioManager) getSystemService(Activity.AUDIO_SERVICE);
+		mAudio.setRingerMode(AudioManager.RINGER_MODE_SILENT);
 	}
 	
-
-	public void sleepRinger(int secondsToSleep) {
-		AudioManager mAudio = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
-		mAudio.setRingerMode(AudioManager.RINGER_MODE_SILENT);
+	public void resetRinger(Intent intent) {
+		AudioManager mAudio = (AudioManager) getSystemService(Activity.AUDIO_SERVICE);
+		mAudio.setRingerMode(AudioManager.RINGER_MODE_NORMAL);
 	}
 
 }
